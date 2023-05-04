99228bbde09959351234f1f480483c57fae1a912
valentinPavon <vpavonlopez02@gmail.com>
2023-05-05 00:51:10 +0200

    Servidor en funcionamiento

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
+var server = require('http').Server(app);
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -38,4 +39,4 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
-module.exports = app;
+module.exports = {app:app,server:server};
